092fd88f3717cf147a16651f0c32b025697f5cae
Anthônia <150256900+1ch1g05@users.noreply.github.com>
2024-07-19 13:42:27 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+    private String nome;
+    private String id;
+
+    public Usuario(String nome, String id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nome='" + nome + '\'' + ", id='" + id + '\'' + '}';
+    }
+}
